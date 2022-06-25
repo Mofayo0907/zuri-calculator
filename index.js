ae1c1a324331c7192e6d01c73d5e279e18bc8b94
Mofayo0907 <103996175+Mofayo0907@users.noreply.github.com>
2022-06-25 15:22:01 +0100

    Introduction commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const myName = "Ogunmodede Joy";
+const myHeight = "5'9''";
+const myCountry = "Nigeria";
+
+alert("My name is " + myName+ "\nI am " + myHeight + " tall" + "\nI am from " + myCountry)
